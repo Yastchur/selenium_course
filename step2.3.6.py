ce62e0212a6f6b4c469b58d2121c81e811abb9cc
Yastchur <Yastchur@gmail.com>
2025-07-14 14:02:27 +0300

    Добавленны файлы

---
+++ step2.3.6.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/redirect_accept.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+try:
+
+    button1 = browser.find_element(By.TAG_NAME, "button")
+    button1.click()
+
+    window_name = browser.window_handles[1]
+    browser.switch_to.window(window_name)
+    
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+
+    answer_element = browser.find_element(By.ID, "answer")
+    answer_element.send_keys(y)
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
